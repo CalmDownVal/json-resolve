b4219d16b013fad9e075a2714845bb1f2b843f89
CalmDownVal <vaclav.lahuta@gmail.com>
2020-01-01 20:44:57 +0100

    remove private methods from typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,8 +7,4 @@ export declare class Resolver
 	public delete(uri: string): boolean;
 	public get(uri: string, root?: Document): Document;
 	public resolve(document: Document, uri?: string): Document;
-
-	private addDocument(document: Document, uri: string): void;
-	private scanSubDocuments(document: Document, baseURI: string, __top?: boolean): void;
-	private resolveRefs(root: Document, document: Document, baseURI: string, __top?: boolean): Document | undefined;
 }
